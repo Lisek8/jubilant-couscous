f92f5228f7d6b311bf7fbb795b104977a652267a
Lisek8 <fafik989@gmail.com>
2022-03-27 13:37:55 +0200

    Added menu bar with 'logo'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MenubarModule } from 'primeng/menubar';
+import { TabViewModule } from 'primeng/tabview';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, MenuBarComponent],
+  imports: [BrowserModule, MenubarModule, TabViewModule],
   providers: [],
   bootstrap: [AppComponent],
 })
