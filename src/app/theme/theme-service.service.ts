384947c24097b05842301208cc6000f56fd23b6d
Lisek8 <fafik989@gmail.com>
2022-03-27 16:39:24 +0200

    Added support for theming

---
+++ src/app/theme/theme-service.service.ts
@@ -0,0 +1,19 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeServiceService {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  switchTheme(theme: string): void {
+    const themeLink = this.document.getElementById(
+      'app-theme'
+    ) as HTMLLinkElement;
+
+    if (themeLink) {
+      themeLink.href = theme + '.css';
+    }
+  }
+}
